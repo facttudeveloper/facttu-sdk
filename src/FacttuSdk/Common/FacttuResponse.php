0eb55bb5faf560fdd9adec48fad31b0271f55de2
luis curo <lacs@syslacs.com>
2020-01-29 19:40:58 -0500

    update adapter resposne

---
+++ src/FacttuSdk/Common/FacttuResponse.php
@@ -0,0 +1,18 @@
+<?php
+namespace FacttuSdk\Common;
+
+use FacttuSdk\Validation\JsonValidator;
+
+
+/**
+ * Class DocumentoSunat
+ *
+ * @package FacttuSdk\Common
+ *
+ * @property string $tipo_documento_sunat
+ * 
+ */
+class FacttuResponse
+{
+	
+}
